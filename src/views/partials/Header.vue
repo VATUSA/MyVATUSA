44cbca97f58b224818017ea6624164b52bcf7cae
Raajheer1 <raaj.patel229@gmail.com>
2024-03-09 01:16:23 -0600

    add notifications

--- src/views/partials/Header.vue
+++ src/views/partials/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-full flex justify-between lg:justify-end border-b border-gray-300 px-10 py-2.5 my-auto text-usa-white lg:text-gray-500 text-xl bg-usa-blue lg:bg-transparent"
+    class="w-full flex justify-between lg:justify-end border-b border-gray-300 px-4 lg:px-10 py-2.5 my-auto text-usa-white lg:text-gray-500 text-xl bg-usa-blue lg:bg-transparent"
   >
     <button
       class="my-auto lg:hidden text-xl leading-10 rounded-md hover:text-usa-red hover:scale-110 transition-all"
@@ -10,15 +10,37 @@
     </button>
     <img src="@/assets/logo-alt.png" class="my-auto block lg:hidden w-28" />
     <div class="my-auto space-x-8">
-      <i class="fa-regular fa-bell hover:text-usa-red lg:hover:text-usa-blue"></i>
+      <div class="relative inline-block text-left">
+        <button type="button" @click="toggleNotification">
+          <i class="fa-regular fa-bell hover:text-usa-red lg:hover:text-usa-blue"></i>
+        </button>
+
+        <div
+          v-if="isNotificationOpen"
+          class="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-64 right-0 top-full mt-2.5"
+        >
+          <ul class="py-2 text-sm text-gray-700 flex flex-col">
+            <li class="py-2 px-4">
+              <div class="flex my-auto justify-between">
+                <div>
+                  <h2 class="text-usa-blue text-xl font-bold">Notifications</h2>
+                </div>
+                <div class="my-auto">
+                  <router-link to="Profile#notifications" class="text-usa-red text-xs font-bold">View All</router-link>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
 
       <div class="relative inline-block text-left">
-        <button type="button" @click="toggleDropdown">
+        <button type="button" @click="toggleProfile">
           <i class="fa-regular fa-user hover:text-usa-red lg:hover:text-usa-blue"></i>
         </button>
 
         <div
-          v-if="isDropdownOpen"
+          v-if="isProfileOpen"
           class="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-64 right-0 top-full mt-2.5"
         >
           <ul class="py-2 text-sm text-gray-700 flex flex-col">
@@ -52,21 +74,37 @@
       </div>
     </div>
 
-    <MobileSidebar :class="isNavOpen ? 'opacity-0 invisible' : 'opacity-100 visible'" @toggle="toggleNav()" />
+    <MobileSidebar :class="!isNavOpen ? 'opacity-0 invisible' : 'opacity-100 visible'" @toggle="toggleNav()" />
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
 import MobileSidebar from "@/views/partials/MobileSidebar.vue";
 
-const isDropdownOpen = ref<boolean>(false);
+const isProfileOpen = ref<boolean>(false);
+const isNotificationOpen = ref<boolean>(false);
 const isNavOpen = ref<boolean>(false);
 
 const toggleNav = (): void => {
+  isProfileOpen.value = false;
   isNavOpen.value = !isNavOpen.value;
 };
 
-const toggleDropdown = (): void => {
-  isDropdownOpen.value = !isDropdownOpen.value;
+const toggleProfile = (): void => {
+  isNavOpen.value = false;
+  isNotificationOpen.value = false;
+  // sleep for 50ms cause im too lazy to actually fix the animation issue
+  setTimeout(() => {
+    isProfileOpen.value = !isProfileOpen.value;
+  }, 50);
+};
+
+const toggleNotification = (): void => {
+  isNavOpen.value = false;
+  isProfileOpen.value = false;
+  // sleep for 50ms cause im too lazy to actually fix the animation issue
+  setTimeout(() => {
+    isNotificationOpen.value = !isNotificationOpen.value;
+  }, 50);
 };
 </script>
